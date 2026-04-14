49a4daf849e30c3ac25bb1ffed79d053ab697c8b
Udayyadla <udaykumar3439@gmail.com>
2026-04-14 13:11:22 +0530

    Refactor serializers to include read-only fields for all models

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,33 +1,44 @@
 from rest_framework import serializers
 from .models import Product, Supplier, Category, Warehouse, Inventory, StockMovement
 
+
+READ_ONLY_FIELDS = ("id", "created_at", "updated_at")
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+        read_only_fields = READ_ONLY_FIELDS
+
+        
 
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
         fields = '__all__'
+        read_only_fields = READ_ONLY_FIELDS
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+        read_only_fields = READ_ONLY_FIELDS
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
         fields = '__all__'
+        read_only_fields = READ_ONLY_FIELDS
 
 class InventorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventory
         fields = '__all__'
+        read_only_fields = READ_ONLY_FIELDS
 class StockMovementSerializer(serializers.ModelSerializer):
     class Meta:
         model = StockMovement
         fields = '__all__'
+        read_only_fields = READ_ONLY_FIELDS
 
     
